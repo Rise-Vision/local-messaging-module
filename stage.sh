2ac16dada9dedd5d7b0f2c67fe9edbfce6e38a7d
Stuart Lees <stuartlees@gmail.com>
2017-10-13 14:08:18 -0400

    Improve to leverage webpack env var for injecting version number for build

--- stage.sh
+++ stage.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 MODULENAME="local-messaging"
-VERSION=$(date +%Y.%m.%d.%H.%M)
+VERSION=$(cat version)
 echo "staging $VERSION"
-echo "$VERSION" >version
 
 git clone git@github.com:Rise-Vision/private-keys.git
 gcloud auth activate-service-account 452091732215@developer.gserviceaccount.com --key-file ./private-keys/storage-server/rva-media-library-ce0d2bd78b54.json
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,31 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MinifyPlugin = require("babel-minify-webpack-plugin");
-const nodeExternals = require('webpack-node-externals');
+const nodeExternals = require("webpack-node-externals");
 const path = require("path");
 const UnzipsfxPlugin = require("unzipsfx-webpack-plugin");
 const ZipPlugin = require("zip-webpack-plugin");
 
-function getVersion(date) {
-  let day = ("0" + date.getDate()).slice(-2),
-    month = ("0" + (date.getMonth() + 1)).slice(-2),
-    year = date.getFullYear(),
-    hour = date.getHours(),
-    minute = date.getMinutes();
+module.exports = env =>  {
 
-  return `${year}.${month}.${day}.${hour}.${minute}`;
-}
-
-module.exports = {
-  entry: "./src/index.js",
-  target: "node",
-  externals: [nodeExternals()],
-  output: {
-    path: path.join(__dirname, "build", "local-messaging", getVersion(new Date())),
-    filename: "index.js"
-  },
-  plugins: [
-    new CopyWebpackPlugin([{from: "./build-temp/node_modules", to: 'node_modules'}]),
-    new MinifyPlugin(),
-    new ZipPlugin({
-      path: path.join(__dirname, "build"),
-      filename: "local-messaging",
-    }),
-    new UnzipsfxPlugin({
-      outputPath: path.join(__dirname, "build"),
-      outputFilename: "local-messaging",
-    })
-  ]
+  return {
+    entry: "./src/index.js",
+    target: "node",
+    externals: [nodeExternals()],
+    output: {
+      path: path.join(__dirname, "build", "local-messaging", env.MODULE_VERSION),
+      filename: "index.js"
+    },
+    plugins: [
+      new CopyWebpackPlugin([{from: "./build-temp/node_modules", to: 'node_modules'}]),
+      new MinifyPlugin(),
+      new ZipPlugin({
+        path: path.join(__dirname, "build"),
+        filename: "local-messaging",
+      }),
+      new UnzipsfxPlugin({
+        outputPath: path.join(__dirname, "build"),
+        outputFilename: "local-messaging",
+      })
+    ]
+  }
 };
