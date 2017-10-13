9b6962d67bbc59f1ff6508ad5e5b706fc951f18f
Stuart Lees <stuartlees@gmail.com>
2017-10-13 10:13:29 -0400

    Build with version as current date and use pre/post bash scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,38 @@
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MinifyPlugin = require("babel-minify-webpack-plugin");
 const nodeExternals = require('webpack-node-externals');
 const path = require("path");
-const pkg = require("./package.json");
 const UnzipsfxPlugin = require("unzipsfx-webpack-plugin");
 const ZipPlugin = require("zip-webpack-plugin");
+
+function getVersion(date) {
+  let day = ("0" + date.getDate()).slice(-2),
+    month = ("0" + (date.getMonth() + 1)).slice(-2),
+    year = date.getFullYear(),
+    hour = date.getHours(),
+    minute = date.getMinutes();
+
+  return `${year}.${month}.${day}.${hour}.${minute}`;
+}
 
 module.exports = {
   entry: "./src/index.js",
   target: "node",
   externals: [nodeExternals()],
   output: {
-    path: path.join(__dirname, "dist", "local-messaging-module", pkg.version),
+    path: path.join(__dirname, "build", "local-messaging", getVersion(new Date())),
     filename: "index.js"
   },
   plugins: [
-    new CleanWebpackPlugin(["dist"], {verbose:  true}),
-    new CopyWebpackPlugin([{from: "./temp/node_modules", to: 'node_modules'}]),
+    new CopyWebpackPlugin([{from: "./build-temp/node_modules", to: 'node_modules'}]),
     new MinifyPlugin(),
     new ZipPlugin({
-      path: path.join(__dirname, "dist"),
-      filename: "local-messaging-module",
+      path: path.join(__dirname, "build"),
+      filename: "local-messaging",
     }),
     new UnzipsfxPlugin({
-      outputPath: path.join(__dirname, "dist"),
-      outputFilename: "local-messaging-module",
+      outputPath: path.join(__dirname, "build"),
+      outputFilename: "local-messaging",
     })
   ]
 };
