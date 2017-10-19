89ba07d88832d942af965b83dfec7f7479e0ae73
rodrigopavezi <rodrigopavezi@gmail.com>
2017-10-19 14:08:48 -0200

    Changing ms to lms

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 const ipc = require("node-ipc"),
   localMessaging = require("./local-messaging.js");
 
-ipc.config.id   = "ms";
+ipc.config.id   = "lms";
 ipc.config.retry = 1500;
 
 localMessaging.init(ipc);
-
-
-
-
