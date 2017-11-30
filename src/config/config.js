1c84c4b3ac3c85aee960e48b1c1c51da5d9705de
Jaime Yoon <jaime@Jaimes-MacBook-Pro.local>
2017-11-30 11:51:19 -0500

    Adds LM specific external-logging function

---
+++ src/config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  moduleName: "local-messaging",
+  bqProjectName: "client-side-events",
+  bqDatasetName: "Module_Events",
+  bqFailedEntryFile: `${module.exports.moduleName}-failed.log`,
+  bqTableName: "local_messaging_events"
+};
