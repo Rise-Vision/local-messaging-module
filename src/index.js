e05878d03701d6b39eee40683703086da44fd832
Tyler Johnson <mail@tyler-johnson.ca>
2018-03-28 09:15:04 -0400

    Facilitate manual testing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+const path = require("path");
 const ipc = require("node-ipc");
 const localMessaging = require("./local-messaging");
 const commonConfig = require("common-display-module");
 const config = require("./config/config");
 const externalLogger = require("./external-logger");
-const modulePath = commonConfig.getModulePath(config.moduleName);
+const modulePath = process.env.NODE_ENV === "manual" ? path.join(__dirname, "..") : commonConfig.getModulePath(config.moduleName);
 const preventBQLog = process.env.RISE_PREVENT_BQ_LOG;
 const util = require("util");
+const forcedVersion = process.env.NODE_ENV === "manual" ? `test-${config.moduleName}` : "";
 
 global.log = require("rise-common-electron").logger(preventBQLog ? null : externalLogger, modulePath, config.moduleName);
 
@@ -20,7 +22,7 @@ function initConfig() {
   return commonConfig.getDisplayId()
     .then(displayId=>{
       config.setDisplayId(displayId);
-      config.setModuleVersion(commonConfig.getModuleVersion(config.moduleName));
+      config.setModuleVersion(forcedVersion || commonConfig.getModuleVersion(config.moduleName));
 
       log.setDisplaySettings({displayid: displayId});
     })
