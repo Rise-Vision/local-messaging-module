ecc95993ccce2ad8eebd45da927e5700c346160b
santiagonoguez <santiago@nul-unu.com>
2018-04-25 11:06:37 -0500

    reorganize websocket unit tests

--- test/unit/websocket.js
+++ test/unit/websocket.js
@@ -1,87 +1,88 @@
 /* eslint-env mocha */
-/* eslint-disable max-statements, no-magic-numbers */
-const assert = require("assert")
-const common = require("common-display-module")
+
+const assert = require("assert");
+const common = require("common-display-module");
 const HttpsProxyAgent = require("https-proxy-agent");
-const Primus = require("primus")
-const simple = require("simple-mock")
+const Primus = require("primus");
+const simple = require("simple-mock");
 
 const websocket = require("../../src/websocket")
 
-describe("Websocket : Unit", ()=>
+describe("Websocket : Unit", () =>
 {
 
-  let socketCreationOptions = null
+  beforeEach(() => {
+    const mockSettings = {displayId: "abc"};
 
-  beforeEach(()=>
-  {
-    const mockSettings = {displayId: "abc"}
+    simple.mock(common, "getMachineId").returnWith("abc");
+    simple.mock(common, "getDisplaySettingsSync").returnWith(mockSettings);
+  });
 
-    simple.mock(common, "getMachineId").returnWith("abc")
-    simple.mock(common, "getDisplaySettingsSync").returnWith(mockSettings)
+  afterEach(() => simple.restore());
 
-    simple.mock(Primus, "createSocket").callFn(()=>
+  describe("createRemoteSocket", () => {
+    let socketCreationOptions = null
+
+    beforeEach(() => {
+      simple.mock(Primus, "createSocket").callFn(() =>
+      {
+        return function(url, options) {
+          socketCreationOptions = options;
+        }
+      });
+    });
+
+    afterEach(() => socketCreationOptions = null);
+
+    it("should create regular websocket when no proxy environment is defined", () => {
+      websocket.createRemoteSocket();
+
+      assert(socketCreationOptions);
+
+      assert.deepEqual(socketCreationOptions.reconnect, {
+        max: 1800000,
+        min: 5000,
+        retries: Infinity
+      });
+
+      assert(!socketCreationOptions.transport);
+    })
+
+    it("should create regular websocket when empty HTTPS_PROXY variable is defined", () => {
+      simple.mock(process.env, 'HTTPS_PROXY', '');
+
+      websocket.createRemoteSocket();
+
+      assert(socketCreationOptions);
+
+      assert.deepEqual(socketCreationOptions.reconnect, {
+        max: 1800000,
+        min: 5000,
+        retries: Infinity
+      });
+
+      assert(!socketCreationOptions.transport);
+    })
+
+    it("should create websocket considering HTTPS_PROXY variable", () =>
     {
-      return function(url, options) {
-        socketCreationOptions = options
-      }
-    })
-  })
+      simple.mock(process.env, 'HTTPS_PROXY', 'http://localhost:9191');
 
-  afterEach(()=> {
-    simple.restore()
-    socketCreationOptions = null
-  })
+      websocket.createRemoteSocket();
 
-  it("should create regular websocket when no proxy environment is defined", ()=>
-  {
-    websocket.createRemoteSocket()
+      assert(socketCreationOptions);
 
-    assert(socketCreationOptions)
+      assert.deepEqual(socketCreationOptions.reconnect, {
+        max: 1800000,
+        min: 5000,
+        retries: Infinity
+      });
 
-    assert.deepEqual(socketCreationOptions.reconnect, {
-      max: 1800000,
-      min: 5000,
-      retries: Infinity
+      assert(socketCreationOptions.transport);
+      assert(socketCreationOptions.transport.agent);
+      assert(socketCreationOptions.transport.agent instanceof HttpsProxyAgent);
     })
 
-    assert(!socketCreationOptions.transport)
-  })
+  });
 
-  it("should create regular websocket when empty HTTPS_PROXY variable is defined", ()=>
-  {
-    simple.mock(process.env, 'HTTPS_PROXY', '')
-
-    websocket.createRemoteSocket()
-
-    assert(socketCreationOptions)
-
-    assert.deepEqual(socketCreationOptions.reconnect, {
-      max: 1800000,
-      min: 5000,
-      retries: Infinity
-    })
-
-    assert(!socketCreationOptions.transport)
-  })
-
-  it("should create websocket considering HTTPS_PROXY variable", ()=>
-  {
-    simple.mock(process.env, 'HTTPS_PROXY', 'http://localhost:9191')
-
-    websocket.createRemoteSocket()
-
-    assert(socketCreationOptions)
-
-    assert.deepEqual(socketCreationOptions.reconnect, {
-      max: 1800000,
-      min: 5000,
-      retries: Infinity
-    })
-
-    assert(socketCreationOptions.transport)
-    assert(socketCreationOptions.transport.agent)
-    assert(socketCreationOptions.transport.agent instanceof HttpsProxyAgent)
-  })
-
-})
+});
